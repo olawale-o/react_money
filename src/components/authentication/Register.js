62e56fb7be1f1855b25c0009e9d66fcfe56afe1d
olawale-o <omoogunolawale@gmail.com>
2021-10-07 16:26:11 +0100

    Add input fields to register form

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const Register = () => (
   <div className="authentication">
-
+    <form>
+      <div className="field">
+        <input className="input" placeholder="First name" />
+      </div>
+      <div className="field">
+        <input className="input" placeholder="Last name" />
+      </div>
+      <div className="field">
+        <input className="input" placeholder="Email" />
+      </div>
+      <div className="field">
+        <input className="input" placeholder="Password" />
+      </div>
+      <div className="field">
+        <input className="input" placeholder="Confirm password" />
+      </div>
+      <input type="submit" value="Create" />
+    </form>
   </div>
 );
 
