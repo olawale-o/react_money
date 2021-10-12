9b3cb62c5a19f92f2653c7cc6323225d407436e1
olawale-o <omoogunolawale@gmail.com>
2021-10-12 15:24:42 +0100

    Style register form

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -2,24 +2,35 @@ import React from 'react';
 
 const Register = () => (
   <div className="authentication">
-    <form>
-      <div className="field">
-        <input className="input" placeholder="First name" />
-      </div>
-      <div className="field">
-        <input className="input" placeholder="Last name" />
-      </div>
-      <div className="field">
-        <input className="input" placeholder="Email" />
-      </div>
-      <div className="field">
-        <input className="input" placeholder="Password" />
-      </div>
-      <div className="field">
-        <input className="input" placeholder="Confirm password" />
-      </div>
-      <input type="submit" value="Create" />
-    </form>
+    <div className="authentication-container">
+      <form className="auth-form">
+        <div className="form-header">
+          <p>Create account</p>
+        </div>
+        <div className="field">
+          <input className="input" placeholder="First name" />
+        </div>
+        <div className="field">
+          <input className="input" placeholder="Last name" />
+        </div>
+        <div className="field">
+          <input className="input" placeholder="Email" />
+        </div>
+        <div className="field">
+          <input className="input" placeholder="Password" />
+        </div>
+        <div className="field">
+          <input className="input" placeholder="Confirm password" />
+        </div>
+        <input type="submit" className="btn btn-secondary" value="Create" />
+        <div className="form-footer">
+          <p>
+            <span>Already have an account? </span>
+            <a href="hi">Log in</a>
+          </p>
+        </div>
+      </form>
+    </div>
   </div>
 );
 
