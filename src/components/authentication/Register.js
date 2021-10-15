611d4e5da481cb24ab2f0dd1caa45c18b7a7dccc
olawale-o <omoogunolawale@gmail.com>
2021-10-15 17:03:24 +0100

    Install formik

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -1,37 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Register = () => (
-  <div className="authentication">
-    <div className="authentication-container">
-      <form className="auth-form">
-        <div className="form-header">
-          <p>Create account</p>
-        </div>
-        <div className="field">
-          <input className="input" placeholder="First name" />
-        </div>
-        <div className="field">
-          <input className="input" placeholder="Last name" />
-        </div>
-        <div className="field">
-          <input className="input" placeholder="Email" />
-        </div>
-        <div className="field">
-          <input className="input" placeholder="Password" />
-        </div>
-        <div className="field">
-          <input className="input" placeholder="Confirm password" />
-        </div>
-        <input type="submit" className="btn btn-secondary" value="Create" />
-        <div className="form-footer">
-          <p>
-            <span>Already have an account? </span>
-            <a href="hi">Log in</a>
-          </p>
-        </div>
-      </form>
+const Register = () => {
+  return (
+    <div className="authentication">
+      <div className="authentication-container">
+        <form className="auth-form">
+          <div className="form-header">
+            <p>Create account</p>
+          </div>
+          <div className="field">
+            <input className="input" placeholder="First name" />
+          </div>
+          <div className="field">
+            <input className="input" placeholder="Last name" />
+          </div>
+          <div className="field">
+            <input className="input" placeholder="Email" />
+          </div>
+          <div className="field">
+            <input className="input" placeholder="Password" />
+          </div>
+          <div className="field">
+            <input className="input" placeholder="Confirm password" />
+          </div>
+          <input type="submit" className="btn btn-secondary" value="Create" />
+          <div className="form-footer">
+            <p>
+              <span>Already have an account? </span>
+              <a href="hi">Log in</a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Register;
