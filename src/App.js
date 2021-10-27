d69bf882d3f9f2dde84a6c66e24c5b97d05f1857
olawale-o <omoogunolawale@gmail.com>
2021-10-27 08:27:31 +0100

    User can visit login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Register from './components/authentication/Register';
 import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/authentication/Login';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route path="/" component={Register} exact />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} exact />
+        <Route path="/dashboard" component={Dashboard} exact />
       </Switch>
     </div>
   );
