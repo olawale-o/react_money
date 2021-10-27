608b705232b04f862499d1b5f26461daf458cd98
olawale-o <omoogunolawale@gmail.com>
2021-10-27 17:34:55 +0100

    Provider store in the root of the application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import Register from './components/authentication/Register';
+import Register from './components/Authentication/Register';
 import Dashboard from './components/Dashboard/Dashboard';
-import Login from './components/authentication/Login';
+import Login from './components/Authentication/Login';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/configureStore';
+
+const store = configureStore;
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
