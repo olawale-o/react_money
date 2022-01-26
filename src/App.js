f00ee9d83a36debbc88df736a524537835cc49ff
olawale-o <omoogunolawale@gmail.com>
2022-01-26 14:26:55 +0100

    Add Styling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import User from './pages/User';
+import Account from './pages/Account';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<User />} exact />
-        <Route path="/dashboard" element={<Dashboard />} exact />
+        <Route path="/" element={<User />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </div>
   );
