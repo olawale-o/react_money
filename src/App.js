b15dcd4f21adcfc9fae9c9d9ef5f3bb0460d3b1c
olawale-o <omoogunolawale@gmail.com>
2022-01-26 03:59:41 +0100

    Change dashboard src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from './pages/Dashboard';
 import User from './pages/User';
 
 function App() {
