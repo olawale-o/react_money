25109abe607b7d05064a7f8bc80d93cf04e69d46
olawale-o <omoogunolawale@gmail.com>
2022-02-06 13:58:49 +0100

    Nest account routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import User from './pages/User';
 import Account from './pages/Account';
+import Savings from './pages/Account/Savings';
 import PrivateRoute from './components/routes/PrivateRoute';
-import PublicRoute from './components/routes/PublicRoute';
+// import PublicRoute from './components/routes/PublicRoute';
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<User />} />
         <Route path="/dashboard" element={(<PrivateRoute><Dashboard /></PrivateRoute>)} />
-        <Route path="/account" element={(<PublicRoute><Account /></PublicRoute>)} />
+        <Route path="account" element={(<PrivateRoute><Account /></PrivateRoute>)}>
+          <Route index element={(<PrivateRoute><Savings /></PrivateRoute>)} />
+          <Route path="savings" element={(<PrivateRoute><Savings /></PrivateRoute>)} />
+        </Route>
       </Routes>
     </div>
   );
