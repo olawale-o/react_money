e120f59c52a85665f5d9ad2b995350c8308c6971
olawale-o <omoogunolawale@gmail.com>
2022-02-06 14:00:27 +0100

    Remove first and last name field

--- src/components/BioDataForm.js
+++ src/components/BioDataForm.js
@@ -2,12 +2,10 @@ import PropType from 'prop-types';
 import MyInput from './shared/MyInput';
 
 const BioDataForm = ({ formField }) => {
-  const { firstName, lastName, phoneNumber } = formField;
+  const { phoneNumber } = formField;
 
   return (
     <>
-      <MyInput type="text" name={firstName.name} placeholder="First Name" />
-      <MyInput type="text" name={lastName.name} placeholder="Last Name" />
       <MyInput type="text" name={phoneNumber.name} placeholder="Phone" />
     </>
   );
@@ -17,12 +15,6 @@ export default BioDataForm;
 
 BioDataForm.propTypes = {
   formField: PropType.shape({
-    firstName: PropType.shape({
-      name: PropType.string.isRequired,
-    }).isRequired,
-    lastName: PropType.shape({
-      name: PropType.string.isRequired,
-    }).isRequired,
     phoneNumber: PropType.shape({
       name: PropType.string.isRequired,
     }).isRequired,
