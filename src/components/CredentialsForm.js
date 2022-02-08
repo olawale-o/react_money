f47aa65c40a2edf9f707fed3ae7ddb3054e0bc61
olawale-o <omoogunolawale@gmail.com>
2022-02-08 20:09:06 +0100

    Modify CredentialsForm

--- src/components/CredentialsForm.js
+++ src/components/CredentialsForm.js
@@ -1,9 +1,44 @@
-const CredentialsForm = () => {
-  console.log('CredentialsForm');
+import { useField } from 'formik';
+import PropType from 'prop-types';
+import MyFile from './shared/MyFile';
+import Preview from './shared/Preview';
 
+const CredentialsForm = ({
+  formField,
+  setFieldValue,
+}) => {
+  const { nepaBill, identity, signature } = formField;
+  const [nepaBillField] = useField(nepaBill);
+  const [identityField] = useField(identity);
+  const [signatureField] = useField(signature);
+  const nepaBillImage = nepaBillField.value;
+  const identityImage = identityField.value;
+  const signatureImage = signatureField.value;
   return (
-    <div>CredentialsForm Works</div>
+    <>
+      <MyFile type="file" name={nepaBill.name} setFieldValue={setFieldValue} />
+      {nepaBillImage && (<Preview file={nepaBillImage} />)}
+      <MyFile type="file" name={identity.name} setFieldValue={setFieldValue} />
+      {identityImage && (<Preview file={identityImage} />)}
+      <MyFile type="file" name={signature.name} setFieldValue={setFieldValue} />
+      {signatureImage && (<Preview file={signatureImage} />)}
+    </>
   );
 };
 
 export default CredentialsForm;
+
+CredentialsForm.propTypes = {
+  formField: PropType.shape({
+    nepaBill: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+    identity: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+    signature: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  setFieldValue: PropType.func.isRequired,
+};
