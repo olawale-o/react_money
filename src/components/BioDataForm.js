de90dd99506d371834203c4b34eb753d01b5cfca
olawale-o <omoogunolawale@gmail.com>
2022-02-08 20:09:26 +0100

    Modify BioDataForm

--- src/components/BioDataForm.js
+++ src/components/BioDataForm.js
@@ -1,12 +1,25 @@
 import PropType from 'prop-types';
 import MyInput from './shared/MyInput';
+import MySelect from './shared/MySelect';
 
 const BioDataForm = ({ formField }) => {
-  const { phoneNumber } = formField;
+  const { account, bank, balance } = formField;
 
   return (
     <>
-      <MyInput type="text" name={phoneNumber.name} placeholder="Phone" />
+      <MySelect name={account.name}>
+        <option value="">Account Type</option>
+        <option value="1">Personal Savings</option>
+        <option value="2">Other Savings</option>
+      </MySelect>
+      <MySelect name={bank.name}>
+        <option value="">Bank Name</option>
+        <option value="1">Access Bank</option>
+        <option value="2">Ecobank</option>
+        <option value="3">Fidelity Bank</option>
+        <option value="4">First Bank</option>
+      </MySelect>
+      <MyInput type="text" name={balance.name} placeholder="Min Amount" />
     </>
   );
 };
@@ -15,7 +28,13 @@ export default BioDataForm;
 
 BioDataForm.propTypes = {
   formField: PropType.shape({
-    phoneNumber: PropType.shape({
+    account: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+    bank: PropType.shape({
+      name: PropType.string.isRequired,
+    }).isRequired,
+    balance: PropType.shape({
       name: PropType.string.isRequired,
     }).isRequired,
   }).isRequired,
