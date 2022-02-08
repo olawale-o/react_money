55cd05d20bb0246ba09b948c464241e7c78783c0
olawale-o <omoogunolawale@gmail.com>
2022-02-08 22:45:53 +0100

    Modify Account routes tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import User from './pages/User';
 import Account from './pages/Account';
 import Savings from './pages/Account/Savings';
+import Current from './pages/Account/Current';
 import PrivateRoute from './components/routes/PrivateRoute';
 // import PublicRoute from './components/routes/PublicRoute';
 
@@ -14,8 +15,9 @@ function App() {
         <Route path="/" element={<User />} />
         <Route path="/dashboard" element={(<PrivateRoute><Dashboard /></PrivateRoute>)} />
         <Route path="account" element={(<PrivateRoute><Account /></PrivateRoute>)}>
-          <Route index element={(<PrivateRoute><Savings /></PrivateRoute>)} />
+          <Route index element={(<PrivateRoute><Navigate to="savings" /></PrivateRoute>)} />
           <Route path="savings" element={(<PrivateRoute><Savings /></PrivateRoute>)} />
+          <Route path="current" element={(<PrivateRoute><Current /></PrivateRoute>)} />
         </Route>
       </Routes>
     </div>
